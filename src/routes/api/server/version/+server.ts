020b25e4f576febc0b459264150cd1dbd37fe831
friendlyFriend4000 <friendlyFriend4000@gmail.com>
2025-01-03 03:12:11 +0100

    changed docker system form using .env to -e flags

--- src/routes/api/server/version/+server.ts
+++ src/routes/api/server/version/+server.ts
@@ -1,25 +1,29 @@
-import { IMMICH_URL } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 
 export async function GET() {
+  const IMMICH_URL = env.IMMICH_URL;
+
+  // Add validation to ensure environment variables are present
+  if (!IMMICH_URL) {
+    throw new Error('Required environment variables IMMICH_API_KEY and IMMICH_URL must be set');
+  }
+
   let response = {};
   const options: ResponseInit = {
     status: 200,
   }
+
   try {
-    // const res = await fetch("https://immich.site.com/api/server/version", {
     const res = await fetch(IMMICH_URL + "/api/server/version", {
       headers: {
-        'Accept': 'application/json',
+        'Accept': 'application/json'
       }
     });
     response = await res.json();
-
   } catch (err) {
     console.error(err);
   }
-
- return new Response(JSON.stringify(response), options)
+  return new Response(JSON.stringify(response), options)
 }
-
 
 
