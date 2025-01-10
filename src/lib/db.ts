02f0c8494e56c1b4e8f55586cf5117b99f4b4c43
friendlyFriend4000 <friendlyFriend4000@gmail.com>
2025-01-10 05:06:23 +0100

    removed env check during building

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,9 +4,10 @@ import { env } from '$env/dynamic/private';
 
 const { Pool } = pkg;
 
-if (!env.DATABASE_URL) {
-	throw new Error('Required environment variable DATABASE_URL must be set');
-}
+
+// if (!env.DATABASE_URL) {
+// 	throw new Error('DB environment variable DATABASE_URL must be set');
+// }
 
 export const pool = new Pool({
 	connectionString: env.DATABASE_URL
