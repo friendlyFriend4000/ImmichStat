7e272f719844b084d98216c798bbc24ad2d00656
friendlyFriend4000 <friendlyFriend4000@gmail.com>
2025-01-12 04:15:11 +0100

    fx indicator placement

--- src/routes/utils/overview/CardServerVersion.svelte
+++ src/routes/utils/overview/CardServerVersion.svelte
@@ -41,7 +41,7 @@
 <Card horizontal class="items-center justify-between" size="xl">
 	<div class="w-full">
 		<p>Immich Version</p>
-		<p class="text-2xl font-bold leading-none text-gray-900 sm:text-3xl dark:text-white">
+		<p class="text-2xl font-bold leading-none text-gray-900 dark:text-white sm:text-3xl">
 			{#if fetchError}
 				{fetchError}
 			{:else if localServerImmichVersion}
@@ -52,14 +52,11 @@
 		</p>
 	</div>
 	{#if isUpdateAvailable()}
-		<Badge
-			color="green"
-			rounded
-			class="w-60 px-2.5 py-0.5"
-			href="https://github.com/immich-app/immich/releases"
-		>
-			<Indicator color="green" size="sm" class="me-1" />
-			<p>Update Available</p>
+		<Badge color="green" rounded class="w-60 px-2.5 py-0.5" href="https://github.com/immich-app/immich/releases">
+			<p class="flex items-center">
+				<Indicator color="green" size="sm" class="me-1" />
+				Update Available
+			</p>
 		</Badge>
 	{:else if localServerImmichVersion && githubReleaseTag}
 		<Badge color="dark" rounded class="w-60 px-2.5 py-0.5">
